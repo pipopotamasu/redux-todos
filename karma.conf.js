6f6cdd98d6f5617cd90ff78f2f276fb0cbb98074
大和村上 <yamatomurakami@daiwamurakami-no-MacBook-Pro.local>
2017-01-16 16:54:54 +0900

    add unit-test

---
+++ karma.conf.js
@@ -0,0 +1,96 @@
+// Karma configuration
+// Generated on Thu Jan 12 2017 23:17:45 GMT+0900 (JST)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    // テスト対象ファイルのベースパスを指定する
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['mocha'],
+
+
+    // list of files / patterns to load in the browser
+    // テスト対象ファイルとテストファイルを指定する
+    files: [
+      'tests/*_test.js',
+    	'tests/**/*_test.js'
+    ],
+
+
+    // list of files to exclude
+    // テストを除外したいファイルを指定する
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    // Jsxなどビルドが必要なファイルをテスト実行前にビルドしてくれるツールを登録する
+    preprocessors: {
+      'tests/*_test.js': ['webpack','sourcemap'],
+      'tests/**/*_test.js': ['webpack','sourcemap']
+    },
+
+    webpack: {
+      devtool: 'inline-source-map',
+      module: {
+        loaders: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+              presets: ['es2015'],
+              plugins: ['babel-plugin-espower']
+            }
+          },
+          {
+            test: /\.json$/,
+            loader: 'json'
+          }
+        ]
+      }
+    },
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['mocha'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_DEBUG,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
