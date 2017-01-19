6c2d0ea8e23bc9b126e6afd53d4891bb7bbce2b1
大和村上 <yamatomurakami@daiwamurakami-no-MacBook-Pro.local>
2017-01-20 02:45:01 +0900

    add Link component test

--- tests/js/components/Link_test.js
+++ tests/js/components/Link_test.js
@@ -1,52 +1,40 @@
 import assert from 'power-assert'
 import React from 'react'
-import TestUtils from 'react-addons-test-utils'
+import { render } from 'react-dom'
+import { shallow } from 'enzyme'
 import Link from '../../../src/js/components/Link'
-
-function renderWithProps (props = {}) {
-  // 仮想DOMを描画する
-  console.log(<Link {...props} />)
-  return TestUtils.renderIntoDocument(<Link {...props} />);
-}
 
 
 describe('Link', function () {
-  let _rendered, _props, _spies;
-
-  beforeEach(function () {
-    _spies = {};
-
-    // propsのモック
-    _props = {
-      // sinon.jsを使用し、関数をspyする
-      onClick: _spies.dispatch = sinon.spy(),
-      children: 'test',
-      active: true
-    };
-
-    _rendered  = renderWithProps(_props);
-  });
-
   // 単純なテキストの表示
-  it('spanタグの描画', function () {
-    const span = TestUtils.scryRenderedDOMComponentsWithTag(_rendered, 'span');
-    assert.equal(span.length, 1)
-    assert.equal(span[0].textContent, 'test')
+  it('spanタグのレンダリング', function () {
+    const onClick = sinon.spy()
+    const active = true
+    const children = 'test'
+    const wrapper = shallow(
+      <Link active={active} onClick={onClick}>{children}</Link>
+    )
+
+    assert.equal(wrapper.find('span').text(), 'test')
   });
 
-  // // propsが描画される
-  // it('<h2>の描画', function () {
-  //   const h2 = TestUtils.scryRenderedDOMComponentsWithTag(_rendered, 'h2');
-  //   expect(h1.length).toEqual(1);
-  //   expect(h1[0].textContent).toEqual('0');
-  // });
-  //
-  // // コンポーネントのテストなので関数が実行されるかどうかまでをテストすればよい
-  // it('Incrementボタンをクリックでdispatchが走ること', function () {
-  //   const btn = TestUtils.scryRenderedDOMComponentsWithTag(_rendered, 'button');
-  //   TestUtils.Simulate.click(btn[0]);
-  //
-  //   // spyされた関数が実行されたかどうかをチェックする
-  //   expect(_spies.dispatch.called).toBe(true);
-  // });
+  it('aタグのレンダリング', () => {
+    // spyでonClickのファンクションをモックする
+    const onClick = sinon.spy()
+    const active = false
+    const children = 'test'
+    // shallowでJSXを擬似的にレンダリングし、DOMオブジェクトを作る
+    const wrapper = shallow(
+      <Link active={active} onClick={onClick}>{children}</Link>
+    );
+
+    // wrapperにレンダリングされたDomオブジェクトからテキストを取り出しアサーション
+    assert.equal(wrapper.find('a').text(), 'test')
+
+    // simulate関数の第二引数で、eventオブジェクトをモックする
+    wrapper.find('a').at(0).simulate('click', { preventDefault: ()=> undefined })
+
+    // spyでモックした関数が呼ばれた回数を比較する
+    assert.equal(onClick.callCount, 1)
+  });
 });
